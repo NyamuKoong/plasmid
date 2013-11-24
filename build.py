d9582713e072f7380de2b17468a9428e3678f89a
Hyungjoon Koo <jimkoo9x@gmail.com>
2013-11-24 21:59:25 +0900

    자동 빌드 - 귀찮으니 폴링식으로

--- build.py
+++ build.py
@@ -1,6 +1,29 @@
 import os
-os.system('coffee -b --compile --output js/ coffee/')
-lesses = os.listdir('./less')
-for less in lesses:
-	less = less.split('.')[0]
-	os.system('lessc ./less/%s.less > ./css/%s.css' % (less, less))
+import sys
+import time
+
+options = {
+	'l' : False # Auto loop
+}
+
+if len(sys.argv) > 1:
+	for argv in sys.argv:
+		if argv in options:
+			options[argv] = True
+
+def build():
+	os.system('coffee -b --compile --output js/ coffee/')
+	lesses = os.listdir('./less')
+	for less in lesses:
+		less = less.split('.')[0]
+		os.system('lessc ./less/%s.less > ./css/%s.css' % (less, less))
+
+	print ('[' + time.strftime('%H:%M:%S') + '] Build')
+
+if options['l']:
+	while True:
+		build()
+		time.sleep(3)
+
+else:
+	build()
